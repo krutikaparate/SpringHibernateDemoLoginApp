953efc40e579a5c4a670f7c05e482f3a3ac04584
krutikaparate <parate.krutika61@gmail.com>
2017-02-01 15:40:18 +0530

    Add files via upload

---
+++ src-main-java-com-springhibernate-demo/form/User.java
@@ -0,0 +1,19 @@
+package com.springhibernate.demo.form;
+
+public class User {
+	   private String userName;
+	   private String email;
+	   
+	   public void setUserName(String userName){
+		      this.userName=userName;
+		  }
+	   public String getUserName(){
+		      return userName;
+	   }	   
+	   public void setEmail(String email){
+		      this.email=email;
+		  }
+	   public String getEmail(){
+		      return email;
+	   }
+	}
